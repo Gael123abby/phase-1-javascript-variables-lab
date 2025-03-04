89ec2aae4bc818aa65aa85bfe6d2f004fb76d7a4
abigaeli <abigaeli.macharia@student.moringaschool.com>
2025-03-04 23:03:43 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file!
+// Code yodescribe('index.js', function () {
+  // there's stuff in here, too})
+  //;ur solution in this file!
+
+it('is set as Scuber', function () {
+// tests are here
+  });
+  it('is set as Scuber', function () {
+    expect(companyName).to.equal('Scuber');
+  });
+  let companyName = 'Scuber';
